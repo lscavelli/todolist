b7c40e95d03bd6f6b76f75e4c34ab6a4abb08f56
LFG Scavelli <newportalclub@gmail.com>
2018-03-01 18:33:03 +0100

    unit test

--- tests/TodolistTest.php
+++ tests/TodolistTest.php
@@ -3,13 +3,15 @@
 namespace Lfgscavelli\Todolist\Test;
 
 use Lfgscavelli\Todolist\Task;
+use Tests\TestCase;
 
 class TodolistTest extends TestCase
 {
     public function testAddition()
     {
         $result = Todolist::add(17, 3);
-
         $this->assertEquals(20, $result);
+        // oppure
+        $this->assertSame(Todolist::add(2, 9), 11);
     }
 }
