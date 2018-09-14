4f46c5c73da965b1980e5d2cc650bf4065d8668c
LFG Scavelli <newportalclub@gmail.com>
2018-09-14 11:21:09 +0200

    Revert "Integrate js Calendar"

--- src/TodoListServiceProvider.php
+++ src/TodoListServiceProvider.php
@@ -15,21 +15,22 @@ class TodoListServiceProvider extends ServiceProvider
     public function boot()
     {
         $todolist = "lfgscavelli/todolist";
-		if (! $this->app->routesAreCached()) {
+        if (! $this->app->routesAreCached()) {
             $this->loadRoutesFrom(__DIR__.'/routes/api.php');
-             $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+            $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         }
+
         // load viste
         $this->loadViewsFrom(__DIR__ . '/resources/views', 'todolist');
-        $this->publishes([
-            __DIR__.'/resources/views' => base_path('resources/views/vendor/'.$todolist),
-        ]);
+        $this->publishes([__DIR__.'/resources/views' => base_path('resources/views/vendor/'.$todolist),]);
+
         // load migrazioni
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        $this->publishes([
-            __DIR__ . '/database/migrations' => database_path('migrations')
-        ], 'migrations');
+        $this->publishes([__DIR__ . '/database/migrations' => database_path('migrations')], 'migrations');
+
+        // altro
         $this->publishes([__DIR__ .'/config/todolist.php' => config_path('todolist.php')], 'config');
+        $this->publishes([__DIR__.'/../public' => public_path('vendor/newportal'),], 'np_public');
     }
 
     /**
